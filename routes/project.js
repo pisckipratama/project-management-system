8ec640f90f10d678fcaf2f2b5f3020859636044b
Piscki Pratama <pisckipratama@gmail.com>
2020-02-21 00:13:19 +0700

    show member for adding project and debug

--- routes/project.js
+++ routes/project.js
@@ -14,7 +14,9 @@ const isLoggedIn = (req, res, next) => {
 module.exports = (pool) => {
   // to landing profile
   router.get('/', isLoggedIn, function (req, res, next) {
-    let sqlGet = `SELECT * FROM projects`
+    let sqlGet = `SELECT * FROM projects`;
+    const body = req.body;
+    console.log(body)
     pool.query(sqlGet, (err, data) => {
       if (err) res.status(500).json(err)
       let result = data.rows.map(item => item)
@@ -22,23 +24,38 @@ module.exports = (pool) => {
         title: 'Dashboard PMS',
         url: 'project',
         user: req.session.user,
+        result,
+        body
+      });
+    })
+  });
+
+  router.post('/', isLoggedIn, function (req, res, next) {
+    const body = req.body;
+    res.redirect('/project')
+  });
+
+  // to add page
+  router.get('/add', isLoggedIn, function (req, res, next) {
+    const sqlAdd1 = `SELECT * FROM users`;
+    pool.query(sqlAdd1, (err, data) => {
+      if (err) res.status(500).json(err);
+      let result = data.rows.map(item => item);
+
+      res.render('project/add', {
+        title: 'Dashboard PMS',
+        user: req.session.user,
+        url: 'project',
         result
       });
     })
   });
 
-  // to add page
-  router.get('/add', isLoggedIn, function (req, res, next) {
-    res.render('project/add', {
-      title: 'Dashboard PMS',
-      user: req.session.user,
-      url: 'project',
-    });
-  });
-
   // masih progress a.k.a belum selesei
   router.post('/add', function (req, res, next) {
     const id = req.params.id;
+    const body = req.body
+    console.log(body)
     let sqlLoad = `SELECT * FROM users WHERE userid=${id}`;
     pool.query(sqlLoad, (err, data) => {
       if (err) res.status(500).json(err)
