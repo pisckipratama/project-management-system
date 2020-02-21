f161d570c33a45e58feb60b4a2ed692409849ae8
Piscki Pratama <pisckipratama@gmail.com>
2020-02-21 16:48:30 +0700

    create delete project feature

--- routes/project.js
+++ routes/project.js
@@ -94,5 +94,15 @@ module.exports = (pool) => {
     })
   })
 
+  // delete project 
+  router.get('/delete/:projectid', isLoggedIn, (req, res, next) => {
+    const projectid = req.params.projectid;
+    let sqlEditProject = `DELETE FROM projects WHERE projectid=${projectid}`;
+
+    pool.query(sqlEditProject, (err) => {
+      if (err) res.status(500).json(err)
+      res.redirect('/project');
+    })
+  })
   return router
 };
