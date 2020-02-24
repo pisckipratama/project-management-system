ef6b26fc3fc895bc61e98a69aea2f6896ca4cf4f
Piscki Pratama <pisckipratama@gmail.com>
2020-02-24 22:21:18 +0700

    filter user feature done

--- routes/users.js
+++ routes/users.js
@@ -5,14 +5,54 @@ module.exports = pool => {
   // main page, filtering data/table, and showing data
   router.get('/', function (req, res, next) {
     let user = req.session.user;
-    let sql = `SELECT users.userid, users.email, CONCAT(users.firstname,' ',users.lastname) AS name, users.position, users.isfulltime FROM users ORDER BY userid`;
+    let sql = `SELECT users.userid, users.email, CONCAT(users.firstname,' ',users.lastname) AS name, users.position, users.isfulltime FROM users`;
+    let result = [];
+    
+    const {checkID, checkName, checkEmail, checkPosition, checkTypeJob, inputId, inputName, inputEmail, inputPosition, inputTypeJob} = req.query;
+
+    if (checkID && inputId) {
+      result.push(`userid=${inputId}`)
+    }
+
+    if (checkName && inputName) {
+      result.push(`CONCAT (users.firstname,' ',users.lastname) LIKE '%${inputName}%'`)
+    }
+
+    if (checkEmail && inputEmail) {
+      result.push(`email='${inputEmail}'`)
+    }
+
+    if (checkPosition && inputPosition) {
+      result.push(`position='${inputPosition}'`)
+    }
+
+    if (checkTypeJob && inputTypeJob) {
+      result.push(`isfulltime=${inputTypeJob == 'Full Time' ? true : false}`)
+    }
+
+    if (result.length > 0) {
+      sql += ' WHERE ';
+      if (result.length > 1) {
+        for (let i = 0; i < result.length; i++) {
+          sql += result[i] + ' AND ';
+        }
+        sql = sql.slice(-(Math.abs(sql.length)), -4);
+      } else {
+        for (let i = 0; i < result.length; i++) {
+          sql += result[i] + ' ';
+        }
+      }
+    }
+    
+    sql += ' ORDER BY userid';
+    console.log(sql);
+
     pool.query(sql, (err, data) => {
       if (err) res.status(500).json(err);
       let result = data.rows.map(item => {
         item.isfulltime = item.isfulltime ? 'Full Time' : 'Part Time'
         return item
       })
-      console.log(result)
       res.render('users/list', {
         title: "Dashboard PMS",
         url: 'users',
@@ -34,7 +74,6 @@ module.exports = pool => {
 
   // post data
   router.post('/add', (req, res, next) => {
-    console.log(req.body);
     const {password, firstname, lastname, email, position} = req.body;
     const isfulltime = req.body.jobtype == 'Full Time' ? true : false;
 
@@ -52,7 +91,6 @@ module.exports = pool => {
     pool.query(sql, (err, data) => {
       if (err) res.status(500).json(err);
       let result = data.rows[0];
-      console.log(result);
       res.render('users/edit', {
         title: "Edit User",
         url: "users",
@@ -72,7 +110,6 @@ module.exports = pool => {
       sql = `UPDATE users SET firstname='${editFirstname}', lastname='${editLastname}', position='${editPosition}', isfulltime=${editJobtype == 'Full Time' ? true : false}, password='${editPassword}' WHERE email='${editEmail}'`
     }
 
-    console.log(sql);
     pool.query(sql, (err, data) => {
       if (err) res.status(500).json(err);
       res.redirect('/users');
@@ -82,8 +119,6 @@ module.exports = pool => {
   router.get('/delete/:userid', (req, res, next) => {
     const {userid} = req.params;
     let sql = `DELETE FROM users WHERE userid=${userid}`;
-
-    console.log(sql)
 
     pool.query(sql, (err) => {
       if (err) res.status(500).json(err)
