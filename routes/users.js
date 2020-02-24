0cb799c0f58c65eb2aa08117d673fc511e210002
Piscki Pratama <pisckipratama@gmail.com>
2020-02-24 16:00:54 +0700

    show user done

--- routes/users.js
+++ routes/users.js
@@ -4,11 +4,22 @@ var router = express.Router();
 module.exports = pool => {
   router.get('/', function (req, res, next) {
     let user = req.session.user;
-    res.render('users/list', {
-      title: "Dashboard PMS",
-      url: 'users',
-      user
-    });
+    let sql = `SELECT users.userid, users.email, CONCAT(users.firstname,' ',users.lastname) AS name, users.position, users.isfulltime FROM users`;
+    pool.query(sql, (err, data) => {
+      if (err) res.status(500).json(err);
+      let result = data.rows.map(item => {
+        item.isfulltime = item.isfulltime ? 'Full Time' : 'Part Time'
+        return item
+      })
+      console.log(result)
+      res.render('users/list', {
+        title: "Dashboard PMS",
+        url: 'users',
+        user,
+        result
+      });
+    })
+
   });
 
   return router;
