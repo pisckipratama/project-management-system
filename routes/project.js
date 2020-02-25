7ecd109c69bb2c0ad4d435dcdfe7e9dfb9ce4fb0
Piscki Pratama <pisckipratama@gmail.com>
2020-02-25 23:35:50 +0700

    show data project and members done

--- routes/project.js
+++ routes/project.js
@@ -12,71 +12,88 @@ const isLoggedIn = (req, res, next) => {
 }
 
 module.exports = (pool) => {
-  // to landing profile
-  router.get('/', isLoggedIn, function (req, res, next) {
-    let sqlGetProjects = `SELECT * FROM projects`;
-    let sqlGetUsers = `SELECT * FROM users`;
-    const body = req.body;
 
-    const {
-      checkID,
-      checkName,
-      checkMember,
-      inputID,
-      inputName,
-      inputMember
-    } = req.query;
-    let content = [];
+  // to landing main page
+  router.get('/', isLoggedIn, function (req, res, next) {
+    const {checkID, checkName, checkMember, inputID, inputName, inputMember} = req.query;
+    const link = (req.url == '/') ? '/?page=1' : req.url;
+    const page = req.query.page || 1;
+    const limit = 3;
+    const offset = (page - 1) * limit;
+    let params = [];
 
     if (checkID && inputID) {
-      content.push = `projectid=${inputID}`
-    }
-    if (checkName && inputName) {
-      content.push = `name='${inputName}'`
-    }
-    if (checkMember && inputMember) {
-      content.push = `member='${inputMember}'`;
+      params.push(`projects.projectid=${inputID}`)
     }
 
-    pool.query(sqlGetProjects, (err, dataProjects) => {
-      if (err) res.status(500).json(err)
-      let dataProject = dataProjects.rows.map(item => item);
-      pool.query(sqlGetUsers, (err, dataUsers) => {
-        if (err) res.status(500).json(err)
-        let dataUser = dataUsers.rows.map(item => item);
-        res.render('project/list', {
-          title: 'Dashboard PMS',
-          url: 'project',
-          user: req.session.user,
-          body,
-          dataUser,
-          dataProject
-        });
+    if (checkName && inputName) {
+      params.push(`project.name='${inputName}'`)
+    }
+
+    if (checkMember && inputMember) {
+      params.push(`members.userid=${inputMember}`)
+    }
+
+    let sql = `SELECT COUNT(id) as total FROM (SELECT DISTINCT projects.projectid AS id FROM projects LEFT JOIN members ON projects.projectid = members.projectid`;
+
+    if (params.length > 0) {
+      sql += ` WHERE ${params.join(" AND ")}`;
+    }
+    sql += `) AS projectmember`;
+
+    pool.query(sql, (err, count) => {
+      if (err) res.status(500).json(err);
+
+      const total = count.rows[0].total;
+      const pages = Math.ceil(total / limit);
+
+      sql = `SELECT DISTINCT projects.projectid, projects.name FROM projects LEFT JOIN members ON projects.projectid = members.projectid`
+
+      if (params.length > 0) {
+        sql += ` WHERE ${params.join(" AND ")}`;
+      } 
+      sql += ` ORDER BY projects.projectid LIMIT ${limit} OFFSET ${offset}`;
+      
+      let subquery = `SELECT DISTINCT projects.projectid FROM projects LEFT JOIN members ON projects.projectid = members.projectid`;
+
+      if (params.length > 0) {
+        subquery += ` WHERE ${params.join(" AND ")}`
+      }
+      subquery += ` ORDER BY projects.projectid LIMIT ${limit} OFFSET ${offset}`;
+
+      let sqlMember = `SELECT projects.projectid, users.userid, CONCAT (users.firstname,' ',users.lastname) AS fullname FROM projects LEFT JOIN members ON projects.projectid = members.projectid LEFT JOIN users ON users.userid = members.userid WHERE projects.projectid IN (${subquery})`;
+
+      console.log(sql);
+      console.log(sqlMember)
+      pool.query(sql, (err, projectData) => {
+        if (err) res.status(500).json(err);
+
+        pool.query(sqlMember, (err, memberData) => {
+          if (err) res.status(500).json(err);
+          
+          projectData.rows.map(project => {
+            project.member = memberData.rows.filter(member => member.projectid == project.projectid).map(data => data.fullname).join(', ').trim()
+          })
+
+          let sqlUser = `SELECT * FROM users`;
+          pool.query(sqlUser, (err, data) => {
+            res.render('project/list', {
+              title: 'Dashboard PMS',
+              url: 'project',
+              user: req.session.user,
+              query: req.query,
+              page,
+              pages,
+              link,
+              dataUser: data.rows.map(item => item),
+              dataProject: projectData.rows.map(item => item)
+            })
+          })
+        })
       })
     })
-
-    // --> dari kang moohat
-    // const {checkID, checkName, checkMember, inputID, inputName, inputMember} = req.query;
-    // const url = (req.url == '/') ? '/?page=1' : req.url;
-    // const page = req.query.page || 1;
-    // const limit = 3;
-    // const offset = (page - 1) * limit;
-    // let params = [];
-
-    // if (checkID && inputID) {
-    //   params.push(`projects.projectid=${inputID}`)
-    // }
-
-    // if (checkName && inputName) {
-    //   params.push(`project.name='${inputName}'`)
-    // }
-
-    // if (checkMember && inputMember) {
-    //   params.push(`members.userid=${inputMember}`)
-    // }
-
-    // let sql = `SELECT COUNT(id)`
   });
+  
 
   // for searching data on progress
   router.post('/', isLoggedIn, function (req, res, next) {
